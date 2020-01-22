acdeb88c7e785280af6b97cd5223c86e92775ba2
Paul Rapulu <rapulu@yahoo.com>
2020-01-22 16:19:31 +0100

    changed paper expiring date on order migration

--- database/migrations/2019_12_11_133510_create_orders_table.php
+++ database/migrations/2019_12_11_133510_create_orders_table.php
@@ -19,7 +19,7 @@ class CreateOrdersTable extends Migration
             $table->bigInteger('product_id');
             $table->string('price');
             $table->string('status')->default('pending');
-            $table->timestamps('expiring_date');
+            $table->date('expiring_date');
             $table->timestamps();
         });
     }
