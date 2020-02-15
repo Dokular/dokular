79b31a0cc0902c9307a4369cd37be7f4048a15da
Paul Rapulu <rapulu@yahoo.com>
2020-02-15 02:26:05 +0100

    login process auth

--- resources/js/views/authenticate.vue
+++ resources/js/views/authenticate.vue
@@ -1,29 +1,40 @@
 <template>
-    <div>
-        processing login
+    <div class="login-auth">
+        <loading
+          :active.sync="isLoading"
+          :is-full-page="true">
+        </loading>
     </div>
 </template>
 <script>
+import Loading from 'vue-loading-overlay';
 import {mapMutations} from 'vuex'
 import axios from 'axios'
 export default {
     props: ['token'],
     data() {
         return {
+            isLoading: false
         }
     },
-
+    components: {
+      Loading
+    },
     methods : {
         ...mapMutations(['SET_TOKEN']),
 
         processLogin() {
+            this.isLoading = true
             axios.post(process.env.MIX_API+'login/verify',{
                token: this.token
             }).then(response => {
+                this.isLoading = false
                 console.log(response.data.token)
                 this.SET_TOKEN(response.data.token)
                 this.$router.push({'name': 'dashboard'})
             }).catch(error => {
+                this.isLoading = false
+                this.$router.push({'name': 'login'})
                 console.log(error)
             })
         }
@@ -35,3 +46,8 @@ export default {
 
 }
 </script>
+<style scoped>
+.login-auth{
+    height: 100vh;
+}
+</style>
