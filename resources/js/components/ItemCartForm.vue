e17081ed05b35e749785c3d838e2966ecc874725
Paul Rapulu <rapulu@yahoo.com>
2020-02-25 03:15:04 +0100

    refactored cart form, created  new component for modal

---
+++ resources/js/components/ItemCartForm.vue
@@ -0,0 +1,161 @@
+<template>
+    <div>
+        <b-modal
+          v-if="category"
+          v-model="showModal"
+          hide-footer
+        >
+            <form-wizard :title="title" :subtitle="subtitle" @on-complete="addToCart">
+                <tab-content title="Vehicle info" :before-change="validateFirstStep">
+                    <div class="form-group">
+                        <label for="cc-payment" class="control-label mb-1">Vehicle make</label>
+                        <input
+                            type="text"
+                            class="form-control"
+                            v-validate="'required'"
+                            data-vv-as="car make"
+                            v-model="cart.make"
+                            name="car_make"
+                            aria-required="true"
+                            aria-invalid="false"
+                            placeholder="E.g Toyota, Kia etc"
+                        >
+                        <span>{{ errors.first('car_make') }}</span>
+                    </div>
+                    <div class="form-group has-success">
+                        <label for="cc-name" class="control-label mb-1">Identification mark</label>
+                        <input
+                          id="cc-name"
+                          type="text"
+                          class="form-control"
+                          v-validate="'required'"
+                          v-model="cart.mark"
+                          name="mark"
+                          data-val="true"
+                        >
+                        <span>{{ errors.first('mark') }}</span>
+                        <span class="help-block field-validation-valid" data-valmsg-for="cc-name" data-valmsg-replace="true"></span>
+                    </div>
+                    <div class="form-group">
+                        <label for="cc-payment" class="control-label mb-1">Name of owner</label>
+                        <input
+                          type="text"
+                          class="form-control"
+                          data-vv-as="car owner name"
+                          v-validate="'required'"
+                          v-model="cart.owner"
+                          name="owner_name"
+                        >
+
+                        <span>{{ errors.first('owner_name') }}</span>
+                    </div>
+                </tab-content>
+                <tab-content title="Papers to renew" :before-change="validateSecondStep">
+                    <b-form-checkbox
+                        v-for="product in category.products"
+                        v-model="cart.products"
+                        :key="product.id"
+                        :value="product"
+                        name="flavour-4a"
+                        inline
+                    >
+                        {{ product.name }}
+                    </b-form-checkbox>
+                    <div class="form-group">
+                        Total: {{ preCartTotal }}
+                    </div>
+                </tab-content>
+            </form-wizard>
+        </b-modal>
+    </div>
+</template>
+<script>
+import {mapMutations, mapActions, mapGetters} from 'vuex'
+export default {
+    props: {
+        show: {
+            type: Boolean,
+            default: false
+        },
+        title: {
+            type: String,
+            default: 'Vehicle paper renewal'
+        },
+        subtitle: {
+            type: String,
+            default: 'Vehicle license, insurance, road worthiness renewal.'
+        },
+        getCategory: {
+            type: Object,
+            default: function() { return {} }
+        },
+    },
+    data() {
+        return{
+            category: null,
+            selected: [],
+            cart: {
+                owner: '',
+                make: '',
+                mark: '',
+                total: '',
+                products: []
+            },
+            base_url: process.env.MIX_APP_URL,
+            showModal: false
+        }
+    },
+    watch: {
+        show(newVal){
+            this.showModal = newVal
+        },
+        getCategory(newVal) {
+            this.category = newVal
+        }
+    },
+    computed: {
+       ...mapGetters(["categories","preCarts"]),
+
+        preCartTotal() {
+            let total = 0;
+            this.cart.products.forEach( cart => {
+                total += Number(cart.price)
+            })
+            return total
+        },
+        checked() {
+            return [
+                this.cart.products.length > 0 || false
+            ]
+        },
+    },
+    methods: {
+        ...mapMutations(['CALCULATE_PRECART_TOTAL']),
+        ...mapActions(["addProductToCart"]),
+        addToCart() {
+            this.cart.total = this.preCartTotal
+            this.addProductToCart(this.cart)
+            this.cart.owner = '',
+            this.cart.make = '',
+            this.cart.mark = '',
+            this.cart.total = '',
+            this.cart.products = []
+            this.showModal = false;
+        },
+        validateSecondStep() {
+            return this.checked[0];
+        },
+        validateFirstStep: function() {
+            return new Promise((resolve, reject) => {
+                this.$validator.validate().then(valid => {
+                    if (!valid) {
+                      reject(false)
+                    }else{
+                      resolve(true)
+                    }
+                });
+            })
+        },
+    }
+}
+</script>
--- resources/js/components/Items.vue
+++ resources/js/components/Items.vue
@@ -27,138 +27,50 @@
         </div>
     </div>
     </div>
-  </div>
-    <b-modal v-if="category" v-model="show" hide-footer >
-        <form-wizard :title="title" :subtitle="subtitle" @on-complete="addToCart">
-            <tab-content title="Vehicle info" :before-change="validateFirstStep">
-                <div class="form-group">
-                    <label for="cc-payment" class="control-label mb-1">Vehicle make</label>
-                    <input
-                        type="text"
-                        class="form-control"
-                        v-model="cart.make"
-                        aria-required="true"
-                        aria-invalid="false"
-                        placeholder="E.g Toyota, Kia etc"
-                    >
-                </div>
-                <div class="form-group has-success">
-                    <label for="cc-name" class="control-label mb-1">Identification mark</label>
-                    <input id="cc-name" type="text" class="form-control" v-model="cart.mark" data-val="true" data-val-required="Please enter the name on card"
-                        autocomplete="cc-name" aria-required="true" aria-invalid="false" aria-describedby="cc-name-error">
-                    <span class="help-block field-validation-valid" data-valmsg-for="cc-name" data-valmsg-replace="true"></span>
-                </div>
-                <div class="form-group">
-                    <label for="cc-payment" class="control-label mb-1">Name of owner</label>
-                    <input type="text" class="form-control" v-model="cart.owner" aria-required="true" aria-invalid="false">
-                </div>
-            </tab-content>
-            <tab-content title="Papers to renew" :before-change="validateSecondStep">
-                <b-form-checkbox
-                    v-for="product in category.products"
-                    v-model="cart.products"
-                    :key="product.id"
-                    :value="product"
-                    name="flavour-4a"
-                    inline
-                >
-                    {{ product.name }}
-                </b-form-checkbox>
-                <div class="form-group">
-                    Total: {{ preCartTotal }}
-                </div>
-            </tab-content>
-            </form-wizard>
-    </b-modal>
+</div>
+  <CartForm
+    :show="showCartForm"
+    :getCategory="category"
+  />
 </div>
 </template>
 
 <script>
-import { ValidationObserver, ValidationProvider } from "vee-validate";
 import {mapMutations, mapActions, mapGetters} from 'vuex'
+import CartForm from '../components/ItemCartForm'
 import Img1 from '../assets/images/depot_img_1.jpg'
 import Img2 from '../assets/images/depot_img_2.jpg'
 import Img3 from '../assets/images/depot_img_3.jpg'
 import BgImg from '../assets/images/depot_hero_1.jpg'
+
 export default {
-    props: {
-        title: {
-            type: String,
-            default: 'Vehicle paper renewal'
-        },
-        subtitle: {
-            type: String,
-            default: 'Vehicle license, insurance, road worthiness renewal.'
-        },
-    },
     data() {
         return{
-            show: false,
+            showCartForm: false,
             img1: Img1,
             img2: Img2,
             img3: Img3,
             bgimg: BgImg,
             category: null,
-            selected: [],
-            cart: {
-                owner: '',
-                make: '',
-                mark: '',
-                total: '',
-                products: []
-            },
             base_url: process.env.MIX_APP_URL,
         }
     },
+
     components: {
-        ValidationObserver,
-        ValidationProvider
+        CartForm
     },
+
     methods: {
-        ...mapMutations(['CALCULATE_PRECART_TOTAL']),
-        ...mapActions([
-            "loadCategory",
-            "addProductToCart"
-        ]),
+        ...mapActions(["loadCategory"]),
 
         openPreCart(category){
             this.category = category
-            this.show = true
-        },
-
-        addToCart() {
-            this.cart.total = this.preCartTotal
-            this.addProductToCart(this.cart)
-            this.cart.owner = '',
-            this.cart.make = '',
-            this.cart.mark = '',
-            this.cart.total = '',
-            this.cart.products = []
-            this.show = false;
-        },
-        validateFirstStep() {
-            return this.$refs.observer.validate()
-        },
-        validateSecondStep() {
-            return this.checked[0];
+            this.showCartForm = true
         }
     },
 
     computed: {
-       ...mapGetters(["categories","preCarts"]),
-
-        preCartTotal() {
-            let total = 0;
-            this.cart.products.forEach( cart => {
-                total += Number(cart.price)
-            })
-            return total
-        },
-        checked() {
-            return [
-                this.cart.products.length > 0 || false
-            ]
-        }
+       ...mapGetters(["categories"]),
     },
 
     created() {
