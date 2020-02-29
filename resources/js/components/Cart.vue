a1487bdd5a05d5eee3783e8053ff221e536db1c7
Paul Rapulu <rapulu@yahoo.com>
2020-02-29 02:00:27 +0100

    fixed FE: cart

--- resources/js/components/Cart.vue
+++ resources/js/components/Cart.vue
@@ -1,32 +1,5 @@
 <template>
   <div class="container" v-if="carts.length">
-      <!-- <div class="card">
-        <div class="card-header">My Cart</div>
-        <table class="table table-bordered">
-          <tbody>
-            <tr v-for="(cart, index) in carts" :key="index">
-              <td>{{ cart.owner }}</td>
-              <td>{{ cart.make }}</td>
-              <td>
-                <p v-for="(product, index) in cart.products" :key="index">
-                {{ product.name }} - {{ product.price }}
-                </p>
-              </td>
-              <td>total: {{ cart.total }} </td>
-              <td><span @click="removeCart(cart)" class="delspan">X</span></td>
-            </tr>
-          </tbody>
-        </table>
-        <div class="card-footer">
-          Total: N{{ total }}
-          <br>
-          <button type="button" class="btn btn-secondary">
-            <router-link tag="a" to="/checkout">
-              Checkout
-            </router-link>
-          </button>
-        </div>
-      </div> -->
     <b-card title="My card">
         <b-card-text v-for="(cart, index) in carts" :key="index">
             <b-container class="bv-example-row">
