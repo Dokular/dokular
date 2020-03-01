52bb56a04415da48d1950cda6d75c08c7d3a6258
Paul Rapulu <rapulu@yahoo.com>
2020-03-01 12:27:32 +0100

    fixed FE: delivery info

--- resources/js/components/CheckOutForm.vue
+++ resources/js/components/CheckOutForm.vue
@@ -74,7 +74,7 @@
             >
                 <option>Choose state</option>
                 <option
-                  value="state"
+                  :value="state"
                   v-for="(state, index) in states"
                   :key="index"
                 >
