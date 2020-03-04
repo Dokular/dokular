32ecef4490324a0ef418a7a962078d40cc4988dd
Paul Rapulu <rapulu@yahoo.com>
2020-03-04 17:43:01 +0100

    lga setup

---
+++ database/factories/LgaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Lga;
+use Faker\Generator as Faker;
+
+$factory->define(Lga::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
