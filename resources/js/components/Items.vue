a57ee6466d4653f1f0ebaf56c275562dc855b9ca
Paul Rapulu <rapulu@yahoo.com>
2020-03-09 01:57:27 +0100

    removed card and refactored col

--- resources/js/components/Items.vue
+++ resources/js/components/Items.vue
@@ -4,25 +4,9 @@
     <div class="site-section ftco-service-image-1 pb-5">
     <div class="container">
         <div class="row">
-        <div class="col-md service text-center "  v-for="(category, index) in categories" :key="index">
-             <b-card
-                title="Card Title"
-                :img-src="category.img"
-                img-alt="Image"
-                img-top
-                tag="article"
-                style="max-width: 20rem;"
-                class="mb-2"
-            >
-                <b-card-text>
-                Some quick example text to build on the card title and make up the bulk of the card's content.
-                </b-card-text>
+        <div class="col-md-4 service text-center mx-auto"  v-for="(category, index) in categories" :key="index">
+            <img :src="category.img" alt="Image" class="img-fluid">
 
-                <b-button href="#" variant="primary">Go somewhere</b-button>
-            </b-card>
-            <!-- <a href="#">
-              <img :src="category.img" alt="Image" class="img-fluid">
-            </a>
             <div class="px-md-3">
             <h3 class="cat-name">{{ category.name }}</h3>
                 <p>
@@ -41,9 +25,10 @@
                     </span>
                     <hr />
                 </span>
-
             </div>
-            <button type="button" class="btn btn-outline-secondary" @click="openPreCart(category)">Renew</button> -->
+            <button type="button" class="btn btn-outline-secondary" @click="openPreCart(category)">
+                Renew
+            </button>
         </div>
         </div>
     </div>
